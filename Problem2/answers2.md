40b4bd0badfd070404a9033fc2e187432dbb67b8
Patrick Sinco <psinco@gmail.com>
2014-03-04 20:08:09 -0600

    progresses on scraping in problem 2

---
+++ Problem2/answers2.md
@@ -0,0 +1,9 @@
+**1.**
+There are two quantitative data types. The year is quantitative-interval, and the populations are quantitative-ratio. 
+
+The population figures are much larger than those we've used. Some of the population figures contain ranges.
+
+**2.**
+$('table.wikitable tbody tr:nth-child(3)') // gets second row (not including header row)
+
+$('table.wikitable tbody tr:not(:nth-child(1)')
--- Problem2/dataExportWiki.js
+++ Problem2/dataExportWiki.js
@@ -18,10 +18,64 @@ $.ajax({
 
     $.each(h2s, function(index, value) {
       //console.log($(this).text());
-      console.log($(value).text()); // print text
+      //console.log($(value).text()); // print text
     });
+    
+    // get the rows we're interested in
+    var popDataRaw = 
+      root.find('table.wikitable tbody tr:nth-child(n+12)');
 
-    saveToFile(['hello world'], 'text.txt');
+    // we'll keep a 2d array of our rows here, csv-ready
+    var popDataFormatted = [];
+
+    var test = [[], []];
+    $.each(popDataRaw, function(index) {
+      var tds = $(this).find('td');
+
+      $.each(tds, function(index) {
+        var year, estCensus, estPopRef, estUn, estHyde, estMadison;
+        if (index % 12 === 0) {
+          test[index].push(convertToInt($(this).text()));
+        } else if (index % 12 === 1) {
+          test[index].push(convertToInt($(this).text()));
+        } else if (index % 12 === 2) {
+
+        }
+      }); // end .each()
+    }); // end .each()
+    console.log(test);
+
+//    $.each(popDataRaw.find('td'), function(cellIndex) {
+//      var numCols = 12;
+//      var year, estCensus, estPopRef, estUn, estHyde, estMadison;
+//      var data = [];
+//
+//        if (cellIndex % numCols === 0) {
+//          data.push(convertToInt($(this).text()));
+//        } else if (cellIndex % numCols === 1) {
+//          //console.log(convertToInt($(this).text()));
+//          //console.log($(this).text() == );
+//          data.push(estCensus = convertToInt($(this).text()));
+//        } else if (cellIndex % numCols === 2) {
+//          data.push(convertToInt($(this).text()));
+//        } else if (cellIndex % numCols === 3) {
+//          data.push(convertToInt($(this).text()));
+//        } else if (cellIndex % numCols === 4) {
+//          data.push(convertToInt($(this).text()));
+//        } else if (cellIndex % numCols === 5) {
+//          data.push(convertToInt($(this).text()));
+//        }
+//      
+//        popDataFormatted.push(data);
+//        //popDataFormatted.push(year + ',' + estCensus + ',' + estPopRef
+//          //+ ',' + estUn + ',' + estHyde + ',' + estMadison);
+//      //}
+//
+//    }); // end .each();
+
+    console.log(popDataFormatted);
+
+    //saveToFile(['hello world'], 'text.txt');
   }, // end success
   error: function() {
     return console.log('error');
@@ -29,7 +83,11 @@ $.ajax({
 }); //end .ajax()
 
 var convertToInt = function(str) {
-  return parseInt(str.replace(/,/g, ""), 10);
+  if (str) {
+    return parseInt(str.replace(/,/g, ""), 10);
+  } else {
+    return 0;
+  }
 };
 
 // takes an array of strings
