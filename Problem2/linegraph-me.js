630bbc8432fd60c84c158ccf0b9c3f68dbb22fdd
Patrick Sinco <psinco@gmail.com>
2014-03-06 19:49:05 -0600

    nothing much

--- Problem2/linegraph-me.js
+++ Problem2/linegraph-me.js
@@ -30,8 +30,14 @@ d3.csv('population-data.csv', function(data) {
   //console.log(data); // debug
   //console.log(data[3].year);// debug
 
+  var color = d3.scale.category10();
+  color.domain(d3.keys(data[0])
+    .filter(function(key) {
+      return key !== 'year';
+    })
+  );
 
-  xScale = d3.scale.linear()
+  var xScale = d3.scale.linear()
     //.domain([0, 100])
     .domain([0, d3.max(function(d) {
       return d.year;
@@ -53,17 +59,27 @@ d3.csv('population-data.csv', function(data) {
     .ticks(5)
     .orient('left')
 
+  // place x-axis
   svg
     .append('g')
     .attr('class', 'x axis')
     .attr('transform', 'translate(40,' + bbVis.h + ')')
     .call(xAxis)  
 
+  // place y-axis
   svg
     .append('g')
     .attr('class', 'y axis')
     .attr('transform', 'translate(40, 0)')
     .call(yAxis)
+
+  var line = d3.svg.line()
+    .x(function(d) {
+      return (d.year);
+    })
+    .y(function(d) {
+      return (d.est);
+    })
 
   var usCensus = [];
   data.forEach(function(d) {
@@ -72,32 +88,43 @@ d3.csv('population-data.csv', function(data) {
 
   var visFrame = svg.append('g')
     .attr('transform', 'translate(' + bbVis.x + ',' 
-      + (bbVis.y + bbVis.h) + ')')
+      + (bbVis.y + bbVis.h) + ')');
     // ...
 
-  visFrame.append('rect')
+//  var estimates = color.domain().map(function(agency) {
+//    return {
+//      agency: agency,
+//      values: data.map(function(d) {
+//        return {
+//          est: 
+//        }
+//      })
+//    };
+//  });
+
+  visFrame.append('rect');
   // ...
 
   //return createVis();
 });
 
 var createVis = function() {
-  xScale = d3.scale.linear()
-    .domain([0, 100])
-    .range([0, bbVis.w]);
-
-
-  var visFrame = svg.append('g')
-    .attr('transform', 'translate(' + bbVis.x + ',' + (bbVis.y + bbVis.h) 
-      + ')')
-    // ...
-
-  visFrame.append('rect')
-  // ...
-
-  var yScale; // define the domain and range; use bbVis
-  var xAxis; 
-  var yAxis;
+//  xScale = d3.scale.linear()
+//    .domain([0, 100])
+//    .range([0, bbVis.w]);
+//
+//
+//  var visFrame = svg.append('g')
+//    .attr('transform', 'translate(' + bbVis.x + ',' + (bbVis.y + bbVis.h) 
+//      + ')')
+//    // ...
+//
+//  visFrame.append('rect')
+//  // ...
+//
+//  var yScale; // define the domain and range; use bbVis
+//  var xAxis; 
+//  var yAxis;
 
   // add y axis to svg
 } // end createVis()
