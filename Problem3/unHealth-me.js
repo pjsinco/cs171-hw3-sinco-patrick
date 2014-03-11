a80763651be2bc494d85502080cb93865a75a60f
pjsinco <psinco@fas.harvard.edu>
2014-03-11 15:12:07 -0500

    gets line-chart to appear in context; properly positions context, focus areas

--- Problem3/unHealth-me.js
+++ Problem3/unHealth-me.js
@@ -1,73 +1,94 @@
 var margin = {
-  top: 50,
-  right: 50,
-  bottom: 50,
-  left: 50
+  //top: 50,
+  //right: 50,
+  //bottom: 50,
+  //left: 50
+  top    : 10,
+  right  : 10,
+  bottom : 100,
+  left   : 40
 };
 
-var width = 960 - margin.left - margin.right;
-var height = 800 - margin.top - margin.top;
-var padding = 50;
-
-var bbOverview = {
-  x: 0,
-  y: 10,
-  w: width,
-  h: 50
+var margin2 = {
+  top    : 430,
+  right  : 10,
+  bottom : 20,
+  left   : 40
 }
+var width = 960 - margin.left - margin.right; // 910
+var height = 500 - margin.top - margin.bottom; // 390
+var height2 = 500 - margin2.top - margin2.bottom; // 50
+//var padding = 50;
+var dataset = [];
 
-var bbDetail = {
-  x: 0,
-  y: 100,
-  w: width,
-  h: 300
-}
+//var bbOverview = {
+//  x: 0,
+//  y: 10,
+//  w: width,
+//  h: 50
+//}
+//
+//var bbDetail = {
+//  x: 0,
+//  y: 100,
+//  w: width,
+//  h: 300
+//}
 
 var parseDate = d3.time.format('%B %Y').parse
 
 var color = d3.scale.category10();
 
 var xScale = d3.time.scale()
-  .range([padding, bbDetail.w])
+  .range([0, width]);
 
-//var xScale2 = d3.time.scale()
-  //.range([0, width])
+var xScale2 = d3.time.scale()
+  .range([0, width]);
 
 var yScale = d3.scale.linear()
-  .range([bbDetail.h, 0])
+  .range([height, 0]);
 
-//var yScale2 = d3.scale.linear()
-  //.range([bbOverview.h, 0])
+var yScale2 = d3.scale.linear()
+  .range([height2, 0]);
 
 var xAxis = d3.svg.axis()
   .scale(xScale)
-  .orient('bottom')
+  .orient('bottom');
 
-//var xAxis2 = d3.svg.axis()
-  //.scale(xScale2)
-  //.orient('bottom')
+var xAxis2 = d3.svg.axis()
+  .scale(xScale2)
+  .orient('bottom');
 
 var yAxis = d3.svg.axis()
   .scale(yScale)
-  .orient('left')
+  .orient('left');
+
+//var yAxis2 = d3.svg.axis()
+  //.scale(yScale2)
+  //.orient('left');
 
 var line = d3.svg.line()
   .x(function(d) {
-    return xScale(d.date);
+    return xScale2(d.date);
   })
   .y(function(d) {
-    return yScale(d.count);
+    return yScale2(d.count);
   })
 
-svg = d3.select('body')
+var svg = d3.select('body')
   .append('svg')
-  .attr('height', height + margin.top + margin.bottom)
+  .attr('height', height + margin.top + margin.bottom) // 
   .attr('width', width + margin.left + margin.right)
 
-svg
-  .append('g')
+var focus = svg.append('g')
+  .attr('class', 'focus')
   .attr('transform', 'translate(' + margin.left + ',' 
     + margin.top + ')');
+
+var context = svg.append('g')
+  .attr('class', 'context')
+  .attr('transform', 'translate(' + margin2.left + ',' 
+    + margin2.top + ')');
 
 d3.csv('unHealth.csv', function(error, data) {
   // set color domain
@@ -81,7 +102,7 @@ d3.csv('unHealth.csv', function(error, data) {
       return parseDate(d.date); 
     }));
 
-  var dataset = color.domain().map(function(cat) {
+  dataset = color.domain().map(function(cat) {
     return {
       cat: cat,
       values: data.map(function(d) {
@@ -92,8 +113,6 @@ d3.csv('unHealth.csv', function(error, data) {
       })
     } 
   });
-
-  console.log(dataset);
 
   yScale.domain([
     d3.min(dataset, function(c) {
@@ -107,19 +126,36 @@ d3.csv('unHealth.csv', function(error, data) {
       });
     })]);
 
-  svg
+  xScale2
+    .domain(xScale.domain());
+
+  yScale2
+    .domain(yScale.domain());
+
+  createVis();
+
+}); // end d3.csv();
+
+function createVis() {
+  focus
     .append('g')
     .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + bbDetail.h + ')')
+    .attr('transform', 'translate(0,' + height + ')')
     .call(xAxis);
 
-  svg
+  focus
     .append('g')
     .attr('class', 'y axis')
-    .attr('transform', 'translate(' + padding + ',0)')
+    //.attr('transform', 'translate(' + padding + ',0)')
     .call(yAxis)
 
-  var categ = svg.selectAll('.category')
+  context
+    .append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height2 + ')')
+    .call(xAxis2)
+    
+  var categ = context.selectAll('.category')
     .data(dataset)
     .enter()
       .append('g')
@@ -135,5 +171,5 @@ d3.csv('unHealth.csv', function(error, data) {
       return color(d.cat);
     })
 
-}); // end d3.csv();
+}
 
