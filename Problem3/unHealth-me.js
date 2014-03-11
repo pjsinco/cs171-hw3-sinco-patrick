7b6662b44451932aaa7fd48ece01ed85d3b735e3
pjsinco <psinco@fas.harvard.edu>
2014-03-10 20:03:52 -0500

    begins to build line chart for problem 3

--- Problem3/unHealth-me.js
+++ Problem3/unHealth-me.js
@@ -22,7 +22,41 @@ var bbDetail = {
   h: 300
 }
 
-dataset = [];
+var parseDate = d3.time.format('%B %Y').parse
+
+var color = d3.scale.category10();
+
+var xScale = d3.time.scale()
+  .range([0, width])
+
+//var xScale2 = d3.time.scale()
+  //.range([0, width])
+
+var yScale = d3.scale.linear()
+  .range([height, 0])
+
+//var yScale2 = d3.scale.linear()
+  //.range([bbOverview.h, 0])
+
+var xAxis = d3.svg.axis()
+  .scale(xScale)
+  .orient('bottom')
+
+//var xAxis2 = d3.svg.axis()
+  //.scale(xScale2)
+  //.orient('bottom')
+
+var yAxis = d3.svg.axis()
+  .scale(yScale)
+  .orient('left')
+
+var line = d3.svg.line()
+  .x(function(d) {
+    return xScale(d.date);
+  })
+  .y(function(d) {
+    return yScale(d.count);
+  })
 
 svg = d3.select('body')
   .append('svg')
@@ -34,10 +68,43 @@ svg
   .attr('transform', 'translate(' + margin.left + ',' 
     + margin.top + ')');
 
-
+var dataset = [];
   
 d3.csv('unHealth.csv', function(error, data) {
+  // set color domain
+  color
+    .domain(d3.keys(data[0]).filter(function(d) {
+      return d !== 'date';
+    }));
 
+  xScale
+    .domain(d3.extent(data, function(d) {
+      return parseDate(d.date); 
+    }));
+
+  dataset = color.domain().map(function(cat) {
+    return {
+      cat: cat,
+      values: data.map(function(d) {
+        return {
+          year: parseDate(d.date),
+          count: +d[cat]
+        };
+      })
+    } 
+  });
+
+  yScale.domain([
+    d3.min(dataset, function(c) {
+      return d3.min(c.values, function(v) {
+        return v.count;
+      });
+    }),
+    d3.max(dataset, function(c) {
+      return d3.max(c.values, function(v) {
+        return v.count;
+      });
+    })]);
 
 
 }); // end d3.csv();
